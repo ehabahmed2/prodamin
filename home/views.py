c87a7c48a4fb6771171565416c43c2fb4999d41c
Ehab <ihapbpc@gmail.com>
2025-07-06 23:52:30 +0200

    updated urls in main and counts

--- home/views.py
+++ home/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from tasks.models import Task
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/main.html', context={})
+    # get total counts for all users
+    users_count = User.objects.count()
+    total_tasks = Task.objects.count()
+    completed_tasks = Task.objects.filter(is_completed=True).count()
+    if total_tasks > 0:
+        completion_rate = round((completed_tasks / total_tasks) * 100, 0)
+    else:
+        completion_rate = 0
+        
+    # personal statistics per each user
+    user = User()
+    if user.is_authenticated:
+        tasks = Task.objects.filter(user=request.user.id)
+        user_tasks_total = tasks.count()
+        user_completed = tasks.filter(is_completed=True).count()
+        user_pending = tasks.filter(is_completed=False).count()
+        
+        recent_activities = tasks.filter(is_completed=True).order_by('-created_at')[:5]
+        
+    context = {
+        # total counts for all users
+        'users_count':users_count,
+        'total_tasks': total_tasks,
+        'completed_tasks': completed_tasks,
+        'completion_rate': completion_rate,
+        # personal statistics per each user
+
+        'user_tasks_total': user_tasks_total,
+        'user_completed': user_completed,
+        'user_pending': user_pending,
+        'recent_activities': recent_activities,
+        
+    }
+    return render(request, 'main/main.html', context=context)
